5c0d17f4907d84aebb8e0d405ed562b7936f226b
Josh <joshmarston@hotmail.co.uk>
2020-03-02 12:50:13 +0000

    updated app and header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
 import Header from "./Header.js";
-
+import Paragraph from "./Paragraph.js";
+import Square from "./Square.js";
 
     const App = () => (
+      // put the header and paragraph elements in empty divs using the <> and </>
+      <>
       <Header />
+      <Paragraph />
+      <Square />
+      </>
       );
-
 
 export default App;
--- src/Header.js
+++ src/Header.js
@@ -2,8 +2,8 @@ import React from "react";
 // we use className to add classes for Bootstrap styling
 
 const Header = () => (
-<header class="jumbotron">
+
 <h1>Hello, world!</h1>
-</header>
+
 );
 export default Header;
