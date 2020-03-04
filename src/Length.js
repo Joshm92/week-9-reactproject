e10a723a5365f02ceab103c1082ec27d3ff05e60
Josh <joshmarston@hotmail.co.uk>
2020-03-04 14:46:45 +0000

    completed Q 1 - length input

---
+++ src/Length.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+
+class Length extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            value: "default input"
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+
+    }
+
+        handleChange(e) {
+            this.setState({ value: e.currentTarget.value });
+        }
+
+        render() {
+            const { value } = this.state;
+            return (
+                <>
+                <form className="form-control">
+                        <p>Length: {value.length}</p>
+                        <input className="form-control" onChange={e => this.handleChange(e)}
+                        value={ value}/>
+                </form>
+                </>
+            );
+        }
+
+}
+
+
+export default Length;
