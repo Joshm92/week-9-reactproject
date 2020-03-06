dbd21fa73a23a6a7261ea80cc40ee4371288758a
Josh <joshmarston@hotmail.co.uk>
2020-03-06 12:29:01 +0000

    Updates

---
+++ precache-manifest.1b3eef8a20c371a97563110d09eb737b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bdf428759ffa70c90af9b0bab2df1fe",
+    "url": "/week-9-reactproject/index.html"
+  },
+  {
+    "revision": "212a8d1aa8cc4c285af9",
+    "url": "/week-9-reactproject/static/css/main.71489e89.chunk.css"
+  },
+  {
+    "revision": "b457f232889a01dd48a7",
+    "url": "/week-9-reactproject/static/js/2.03f4db4b.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/week-9-reactproject/static/js/2.03f4db4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "212a8d1aa8cc4c285af9",
+    "url": "/week-9-reactproject/static/js/main.fed4004b.chunk.js"
+  },
+  {
+    "revision": "0a4697881e9a5d9913a7",
+    "url": "/week-9-reactproject/static/js/runtime-main.3a421668.js"
+  }
+]);
